17c973a040bad8e8b1a1bf2bd6bb92ac32cf0315
Xanderath <alexgiller@proton.me>
2025-12-02 23:16:50 -0500

    added downloading butler from godot

--- addons/itchio_uploader/itchio_uploader.gd
+++ addons/itchio_uploader/itchio_uploader.gd
@@ -42,3 +42,47 @@ func itchIoLogin()->void:
 func openSettings()->void:
 	var popup=itchSettingsPopup.instantiate()
 	add_child(popup)
+	
+func downloadButler()->void:
+	const baseURL='https://broth.itch.zone/butler/{1}/LATEST/archive/default'
+	var osURLName:String
+	match OS.get_name():
+		"Windows":
+			# screw it, assume 64-bits :,(
+			osURLName='windows-amd64'
+		"MacOS":
+			osURLName='darwin-amd64'
+		"Linux":
+			osURLName="linux-amd64"
+	var url=baseURL.format([osURLName])
+	var request=HTTPRequest.new()
+	add_child(request)
+	request.request_completed.connect(downloadFinished)
+	var error=request.request(url)
+	if error!=OK:
+		print("An error occured downloading butler")
+
+func downloadFinished(result, response_code, headers, body)->void:
+	const butlerFolder='res://addons/itchio_uploader/butler/'
+	const zipFilePath=butlerFolder+'butler.zip'
+	# write file
+	var file=FileAccess.open(zipFilePath,FileAccess.WRITE)
+	file.store_buffer(body)
+	file.close()
+	
+	var zipReader=ZIPReader.new()
+	zipReader.open(zipFilePath)
+	
+	for zipFile in zipReader.get_files():
+		var currFile=FileAccess.open(butlerFolder+zipFile,FileAccess.WRITE)
+		currFile.store_buffer(zipReader.read_file(zipFile))
+		currFile.close()
+
+	var butlerFile:String
+	if OS.get_name()!='Windows':
+		butlerFile='butler'
+		FileAccess.set_unix_permissions(butlerFolder+'butler',FileAccess.UNIX_EXECUTE_OWNER)
+	else:
+		butlerFile='butler.exe'
+	#Store butler path
+	ItchSettings.setSetting(3,butlerFolder+butlerFolder)
